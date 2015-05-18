5c8ff6c547f33ab6a7740e2a2f511a6e47977f07
Diana Mincu <diana.p.mincu@gmail.com>
2015-05-18 20:16:08 +0300

    Deleted useless stuff

--- src/ReactiveAgent.java
+++ src/ReactiveAgent.java
@@ -1,5 +1,28 @@
 import jade.core.*;
+import jade.core.behaviours.SimpleBehaviour;
 
 public class ReactiveAgent extends GenericAgent {
+	
+	protected void setup() {
+		System.out.println("Hello. My name is " + this.getLocalName());
+		
+		addBehaviour(new ReactiveResponderBehaviour());
+	}
 
+}
+
+class ReactiveResponderBehaviour extends SimpleBehaviour {
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
