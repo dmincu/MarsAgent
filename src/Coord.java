2cf6d7ce6ad8fa59270927f6e1d548e18a2d396f
Florin Brad <florin.brad42@gmail.com>
2015-05-22 15:36:48 +0300

    Added cognitive agent behaviour

--- src/Coord.java
+++ src/Coord.java
@@ -13,5 +13,45 @@ public class Coord {
 		this.x = x;
 		this.y = y;
 	}
-
+	
+	/* Returns the direction an agent need to take in order
+	 * to reach target from source 
+	 0 - already there
+	 * 1 - UP
+	 * 2 - LEFT
+	 * 3 - RIGHT
+	 * 4 - DOWN
+	 */
+	public int getDirectionTo(Coord target) {
+		if (x == target.x && y == target.y) {
+			return 0;
+		}
+		if (x < target.x) {
+			return 3;
+		} else if (x > target.x) {
+			return 2;
+		} else if (y < target.y) {
+			return 1;
+		} else {
+			return 4;
+		}
+	}
+	
+	public Coord getNextCoord(int direction) {
+		/* UP */
+		if (direction == 1) {
+			return new Coord(x, y + 1);
+		} else if (direction == 2) { // LEFT
+			return new Coord(x - 1, y);
+		} else if (direction == 3) { // RIGHT
+			return new Coord(x + 1, y);
+		} else if (direction == 4) { // DOWN
+			return new Coord(x, y - 1);
+		} else 
+			return new Coord(x, y);
+	}
+	
+	public String toString() {
+		return "[" + x + ", " + y + "]";
+	}
 }
